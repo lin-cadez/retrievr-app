df18c2cd8afa94c5fc8aca0b51afec7b78f16e15
lin-cadez <lin.cadez@gmail.com>
2024-09-19 15:31:14 +0200

    Refactor background gradient in retrievr-home.tsx and wani.css

--- src/components/retrievr-home.tsx
+++ src/components/retrievr-home.tsx
@@ -28,7 +28,7 @@ export default function RetrievrHome() {
         </nav>
       </header>
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gradient-to-b from-blue-50 to-white dark:from-gray-900 dark:to-gray-800">
+        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 light-blue-gradient  dark:from-gray-900 dark:to-gray-800">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
